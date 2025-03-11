d89653feb83321e8dead42f467512e843161f770
yuvaraj3377 <yuvarajvpm143@gmail.com>
2025-03-11 12:36:09 +0530

    Add files via upload

---
+++ code/src/App.jsx
@@ -0,0 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Songs from './Components/Songs'
+import Sidebar from './Components/Sidebar'
+import Favorities from './Components/Favorities'
+import Playlist from './Components/Playlist';
+
+
+function App() {
+ 
+  return (
+   <div  >
+    <BrowserRouter>
+    <div>
+    <Sidebar/>
+    </div>
+ 
+          <div>
+          <Routes>
+            <Route path='/songs' element={<Songs/>} />
+            <Route path='/favorities' element={<Favorities/>} />
+            <Route path='/playlist' element={<Playlist/>} />
+           </Routes>
+          </div>
+        </BrowserRouter>
+    
+   </div>
+  )
+}
+
+export default App
